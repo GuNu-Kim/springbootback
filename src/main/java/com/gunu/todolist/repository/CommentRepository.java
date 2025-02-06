7846a3716ba9233e2d4c816bd0e8ae805047ea47
gunu_home <kkw7654321@gmail.com>
2025-02-06 21:51:03 +0900

    [SpringBoot+Reactjs(ts)+MySQL] 55~56강 반영 게시물 상세 페이지 좋아요, 댓글 작성, 게시물 삭제 API 연동

--- src/main/java/com/gunu/todolist/repository/CommentRepository.java
+++ src/main/java/com/gunu/todolist/repository/CommentRepository.java
@@ -5,6 +5,7 @@ import com.gunu.todolist.repository.resultSet.GetCommentListResultSet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -24,4 +25,7 @@ public interface CommentRepository extends JpaRepository<CommentEntity, Integer>
             nativeQuery=true)
 
     List<GetCommentListResultSet> getCommentList(Integer boardNumber);
+
+    @Transactional
+    void deleteByBoardNumber(Integer boardNumber);
 }
